a7577911d719613bfc682e236091c24f1531cd64
MinexAutomation <minex.automation@gmail.com>
2019-10-18 16:10:28 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Pictia.Frisia.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Pictia.Frisia.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Pictia.Frisia/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Pictia.Frisia
+{
+    public class Class1
+    {
+    }
+}
