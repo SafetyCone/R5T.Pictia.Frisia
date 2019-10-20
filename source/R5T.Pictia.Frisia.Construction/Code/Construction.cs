11c07508a56d2636632c9cc15b21f9767ec5f39b
MinexAutomation <minex.automation@gmail.com>
2019-10-20 08:24:44 -0700

    Added test of getting a connected SFTP client wrapper.

---
+++ source/R5T.Pictia.Frisia.Construction/Code/Construction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Pictia.Frisia.Construction
+{
+    public static class Construction
+    {
+        public static void SubMain()
+        {
+            Construction.TestGetConnectedSftpClientWrapper();
+        }
+
+        private static void TestGetConnectedSftpClientWrapper()
+        {
+            var serviceProvider = Program.GetServiceProvider();
+
+            using (var sftpClientWrapper = serviceProvider.GetRequiredService<SftpClientWrapper>())
+            {
+                Console.WriteLine($"Is connected: {sftpClientWrapper.SftpClient.IsConnected}");
+            }
+        }
+    }
+}
