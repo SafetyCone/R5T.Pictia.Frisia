78d1aa0d4ae130ae9ba8255d7300f75e0073bbea
MinexAutomation <minex.automation@gmail.com>
2019-10-21 12:27:03 -0700

    Added test of executing a remote command using an SFTP client.

--- source/R5T.Pictia.Frisia.Construction/Code/Construction.cs
+++ source/R5T.Pictia.Frisia.Construction/Code/Construction.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 
 using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Pictia.Extensions;
 
 
 namespace R5T.Pictia.Frisia.Construction
@@ -9,7 +12,24 @@ namespace R5T.Pictia.Frisia.Construction
     {
         public static void SubMain()
         {
-            Construction.TestGetConnectedSftpClientWrapper();
+            //Construction.TestGetConnectedSftpClientWrapper();
+            //Construction.TestGetOutputOfSshCommand("PATH GOES HERE!"); // Note: public!!! Be careful not to check-in identifying paths!
+        }
+
+        private static void TestGetOutputOfSshCommand(string directoryPath)
+        {
+            var serviceProvider = Program.GetServiceProvider();
+
+            using (var sftpClientWrapper = serviceProvider.GetRequiredService<SftpClientWrapper>())
+            using (var sshClientWrapper = sftpClientWrapper.GetSshClientWrapper())
+            {
+                var commandText = $"find \"{directoryPath}\" -print -ls";
+                using (var command = sshClientWrapper.SshClient.CreateCommand(commandText))
+                {
+                    var output = command.Execute();
+                    Console.WriteLine(output);
+                }
+            }
         }
 
         private static void TestGetConnectedSftpClientWrapper()
